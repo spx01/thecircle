f9bb5a7ddf260816a670741ccb3df0b6d35bf3d7
spx01 <42381535+spx01@users.noreply.github.com>
2022-01-06 17:04:37 +0200

    Update cached emojis on emojiUpdate

--- src/events/common.ts
+++ src/events/common.ts
@@ -13,6 +13,13 @@ export abstract class AppDiscord {
   }
 
   /* eslint-disable */
+  @On('emojiUpdate')
+  async emojiUpdate([oldEmoji, newEmoji]: ArgsOf<'emojiUpdate'>, client: Client) {
+    /* eslint-enable */
+    await freemotes.updateEmojis(oldEmoji.guild);
+  }
+
+  /* eslint-disable */
   @On('messageCreate')
   async messageCreate([message]: ArgsOf<'messageCreate'>, client: Client) {
     /* eslint-enable */
