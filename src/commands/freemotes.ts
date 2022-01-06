6af8113a839133d5bfc06a26cced62aea0708629
spx01 <42381535+spx01@users.noreply.github.com>
2022-01-06 20:09:31 +0200

    freemotes: add external emoji permission check

--- src/commands/freemotes.ts
+++ src/commands/freemotes.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, GuildChannel } from 'discord.js';
 import { Discord, Slash } from 'discordx';
 import { freemotes } from '../main.js';
 
@@ -10,6 +10,21 @@ class FreemoteCommands {
     interaction: CommandInteraction
   ) {
     /* eslint-enable */
+    const { guild } = interaction;
+    const everyone = guild?.roles.everyone;
+    if (!everyone) {
+      return;
+    }
+    const channel = interaction.channel as GuildChannel;
+    const perms = channel.permissionsFor(guild?.roles.everyone);
+    if (!perms?.has('USE_EXTERNAL_EMOJIS')) {
+      interaction.reply({
+        ephemeral: true,
+        content: 'Error: Cannot use external emojis in this channel.',
+      });
+      return;
+    }
+
     await interaction.deferReply();
     await interaction.editReply(freemotes.getEmoteList());
   }
